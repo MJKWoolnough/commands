ce94a62c305bdee5f70761e65d9d9afdde225af0
Michael Woolnough <michael.woolnough@gmail.com>
2026-08-13 22:22:41 +0100

    added global help func

--- commands.sh
+++ commands.sh
@@ -1,18 +1,50 @@
 #!/bin/bash
 
+__args=("$@");
+
 sections() {
 	declare start="${1:?Start string required}";
 
-	__handleParts "grep '^$start' ${0@Q} | cut -b'$(( ${#start} + 1))-'"  "sed -n -e '/^$start'\$part'$/,/^--/{//!p}' ${0@Q}";
+	__handleParts "grep '^$start' ${0@Q} | cut -b'$(( ${#start} + 1))-' | grep -v '^$'"  "sed -n -e '/^$start'\${part:-}'$/,/^--/{//!p}' ${0@Q}";
+}
+
+__description() {
+	sed -n '/^#/!q; p' | sed -e 's/^# *//';
+}
+
+__help() {
+	declare partsFn="$1";
+	declare sectionFn="$2";
+
+	echo "Usage: $0 [--help] SUBCOMAND";
+	
+	eval "$sectionFn" | __description;
+
+	echo;
+
+	declare maxLength="$(eval "$partsFn" | wc -L)";
+
+	echo "Subcommands:";
+
+	while read part; do
+		printf "  %-${maxLength}s" "$part";
+		echo -n "  ";
+
+		echo "$(eval "$sectionFn" | __description)";
+	done < <(eval "$partsFn");
 }
 
 __handleParts() {
 	declare partsFn="$1";
 	declare sectionFn="$2";
 
-	echo "$sectionFn";
+	if [ "${__args[0]:-}" = "--help" ]; then
+		__help "$partsFn" "$sectionFn";
+
+		exit 0;
+	fi;
+
 	while read part; do
-		echo "$part";
-		eval "$sectionFn" <<<"$part";
+		eval "$sectionFn";
 	done < <(eval "$partsFn");
 }
