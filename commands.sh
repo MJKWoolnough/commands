2e4e44f27709c367f66b8396895066d44202b3a1
Michael Woolnough <michael.woolnough@gmail.com>
2026-08-13 22:03:54 +0100

    use eval commands to dynamically get section titles and section data

--- commands.sh
+++ commands.sh
@@ -3,5 +3,16 @@
 sections() {
 	declare start="${1:?Start string required}";
 
-	grep "^$start" $0 | cut -b"$(( ${#start} + 1))-";
+	__handleParts "grep '^$start' ${0@Q} | cut -b'$(( ${#start} + 1))-'"  "sed -n -e '/^$start'\$part'$/,/^--/{//!p}' ${0@Q}";
+}
+
+__handleParts() {
+	declare partsFn="$1";
+	declare sectionFn="$2";
+
+	echo "$sectionFn";
+	while read part; do
+		echo "$part";
+		eval "$sectionFn" <<<"$part";
+	done < <(eval "$partsFn");
 }
